ff6c9af94d238f8d59eff5eb1cce4ed5586f9a20
Majap <79178863+Minemagazine@users.noreply.github.com>
2022-02-05 23:35:18 +0900

    Update kbotinfo.py

--- BotInfo/kbotinfo.py
+++ BotInfo/kbotinfo.py
@@ -1,1 +1,9 @@
+import requests, json
+botID = int(input("Bot ID: "))
+headers = {}
+url = 'https://koreanbots.dev/api/v2/bots/932196994775121930'
+
+response = requests.get(url, headers=headers)
+data = response.json()
+print(data)
 
